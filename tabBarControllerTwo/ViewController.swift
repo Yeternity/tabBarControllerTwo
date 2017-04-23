d143662537aa9d497512dbac61cf77a430c1d635
seirra <hack@seirradeMac.local>
2017-04-23 17:36:21 +0800

    Initial Commit

---
+++ tabBarControllerTwo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tabBarControllerTwo
+//
+//  Created by seirra on 2017/4/23.
+//  Copyright © 2017年 seirra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
